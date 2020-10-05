a65377a1c156ae2c3c8b87c60945a54174292d97
Tulle12 <72298643+Tulle12@users.noreply.github.com>
2020-10-05 19:34:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.soundLevel())
 })
+input.onButtonPressed(Button.B, function () {
+    basic.setLedColor(0x00ff00)
+})
 basic.forever(function () {
     if (input.soundLevel() > 20) {
         basic.setLedColor(0xff0000)
@@ -11,4 +14,5 @@ basic.forever(function () {
     input.soundLevel(),
     100
     )
+    basic.pause(5000)
 })
